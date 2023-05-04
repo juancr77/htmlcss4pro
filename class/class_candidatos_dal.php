9cf51fa675c4feab1df744832423e43ea8e1f69e
juancr77 <cruz_j@uadec.edu.mx>
2023-05-04 16:35:28 -0600

    en horas de servicio

--- class/class_candidatos_dal.php
+++ class/class_candidatos_dal.php
@@ -14,13 +14,18 @@ function __destruct(){
 parent :: __destruct();
 }//destruct
 
-public datos_por_rfc($rfc){
+public datos_por_rfc($rfc){//inicio dato por rfc
     $rfc = $this->db_conn->real_escape_string($rfc);
     $sql = "select * from candidatos where rfc='$rfc'";
     $this -> set_sql($sql);
     $result = mysqli_query($this->db_conn,$this->db_query)
-    or die (mysqli)
-}
+    or die (mysqli_error($this->db_conn));
+
+    $total_rfc=mysqli_num_rows($result);
+    $obj_det=null;
+
+
+}//datos por rfc
 
 
 } <!-- fin del cand -->
